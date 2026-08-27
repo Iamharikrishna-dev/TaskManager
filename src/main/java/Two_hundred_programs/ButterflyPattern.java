3cc49ed95359f28f553239ba91ba41bb45af63a3
Harikrishna <iamharikrishna@icloud.com>
2026-08-27 13:18:02 +0530

    doc upload

--- src/main/java/Two_hundred_programs/ButterflyPattern.java
+++ src/main/java/Two_hundred_programs/ButterflyPattern.java
@@ -1,3 +1,17 @@
 package Two_hundred_programs;
 
-public class ButterflyPattern { public static void main(String[] args) { int n=5; for(int i = 1; i<=n; i++){for(int j = 1; j<=i; j++)System.out.print("*");for(int s = 1; s<=2*(n-i); s++)System.out.print(" ");for(int j = 1; j<=i; j++)System.out.print("*");System.out.println();} for(int i = n-1; i>=1; i--){for(int j = 1; j<=i; j++)System.out.print("*");for(int s = 1; s<=2*(n-i); s++)System.out.print(" ");for(int j = 1; j<=i; j++)System.out.print("*");System.out.println();} } }
+public class ButterflyPattern {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++)System.out.print("*");
+            for(int s = 1; s<=2*(n-i); s++)System.out.print(" ");
+            for(int j = 1; j<=i; j++)System.out.print("*");System.out.println();
+        } for(int i = n-1; i>=1; i--){
+            for(int j = 1; j<=i; j++)System.out.print("*");
+            for(int s = 1; s<=2*(n-i); s++)System.out.print(" ");
+            for(int j = 1; j<=i; j++)System.out.print("*");
+            System.out.println();
+        }
+    }
+}
